8f39dee2501a738c9850c19bb8ca3984fa51e8d8
Asra Ali <asraa@google.com>
2022-06-21 13:58:18 -0500

    fixup
    Signed-off-by: Asra Ali <asraa@google.com>

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"flag"
-	"fmt"
 	"log"
 	"os"
 
@@ -14,8 +13,6 @@ var input = flag.String("input", "test.yaml", "input YAML file to parse")
 // This is a test program that parses an input YAML file.
 func main() {
 	flag.Parse()
-
-	fmt.Printf("::set-output name=egg::run-me-at-the-attested-branch\n")
 
 	file, err := os.ReadFile(*input)
 	if err != nil {
