ef52ff1a1f65edb2b13aa493432136e9b0fe5141
Asra Ali <asraa@google.com>
2022-06-21 11:33:50 -0500

    add input file
    Signed-off-by: Asra Ali <asraa@google.com>

--- main.go
+++ main.go
@@ -1,14 +1,20 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"os"
 
 	"github.com/asraa/slsa-example/parse"
 )
 
+var input = flag.String("input", "test.yaml", "input YAML file to parse")
+
+// This is a test program that parses an input YAML file.
 func main() {
-	file, err := os.ReadFile("test.yaml")
+	flag.Parse()
+
+	file, err := os.ReadFile(*input)
 	if err != nil {
 		log.Fatal(err)
 	}
